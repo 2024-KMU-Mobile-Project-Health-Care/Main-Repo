54a1b300dde6c31a3bc85524b5d579a44341a7b7
sjs <wordok38@gmail.com>
2024-11-03 17:32:20 +0900

    add and declare class : PainInfo.java

---
+++ app/src/main/java/com/example/healthcareproject/PainInfo.java
@@ -0,0 +1,26 @@
+package com.example.healthcareproject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PainInfo {
+    private String painType;
+    private List<float[]> coordinates;
+
+    public PainInfo(String painType) {
+        this.painType = painType;
+        this.coordinates = new ArrayList<>();
+    }
+
+    public String getPainType() {
+        return painType;
+    }
+
+    public List<float[]> getCoordinates() {
+        return coordinates;
+    }
+
+    public void addCoordinate(float x, float y) {
+        coordinates.add(new float[]{x, y});
+    }
+}
